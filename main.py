d156c0fd7956e60b99e8c8ab0a90c15de50393f6
Tze Hng Loke <tzehng@botmd.io>
2022-06-12 21:02:10 +0800

    basic setup

---
+++ main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import requests
+
+api_key = ""
+domain = "https://api.mailgun.net/v3/sandbox0d2217c926064a2d944389872026616f.mailgun.org"
+
+def send_simple_message ():
+	return requests.post(
+		f"{domain}/messages",
+		auth=("api", api_key),
+		data={"from": "Tze Hng <mailgun@sandbox0d2217c926064a2d944389872026616f.mailgun.org>",
+			"to": ["tloke@andrew.cmu.edu"],
+			"subject": "Hello",
+			"text": "Testing some Mailgun awesomness!"})
+
+
+def send_template_message():
+	return requests.post(
+		f"{domain}/messages",
+		auth=("api", api_key),
+		data={"from": "Tze Hng <mailgun@sandbox0d2217c926064a2d944389872026616f.mailgun.org>",
+			"to": ["tloke@andrew.cmu.edu"],
+			"subject": "Hello",
+			"template": "testing",
+			"v:test" : "test"
+			})
+
+
+send_template_message()
+
