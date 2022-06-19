2c317cc9cc22139460c6c5bcfa375a127a63d548
Tze Hng Loke <tzehng@botmd.io>
2022-06-19 16:48:43 +0800

    added python script to send emails for Outreach

--- SendOperations.py
+++ SendOperations.py
@@ -1,31 +1,25 @@
 #!/usr/bin/env python
 import requests
-import secret
 
-api_key = secret.api_key
-domain = "https://api.mailgun.net/v3/sandbox0d2217c926064a2d944389872026616f.mailgun.org"
-
-def send_simple_message ():
+def send_simple_message (recipientList, api_key, domain):
 	return requests.post(
 		f"{domain}/messages",
 		auth=("api", api_key),
 		data={"from": "Tze Hng <mailgun@sandbox0d2217c926064a2d944389872026616f.mailgun.org>",
-			"to": ["tloke@andrew.cmu.edu"],
+			"to": recipientList,
 			"subject": "Hello",
 			"text": "Testing some Mailgun awesomness!"})
 
 
-def send_template_message():
+def send_template_message (recipientList, api_key, domain):
 	return requests.post(
 		f"{domain}/messages",
 		auth=("api", api_key),
 		data={"from": "Tze Hng <mailgun@sandbox0d2217c926064a2d944389872026616f.mailgun.org>",
-			"to": ["tloke@andrew.cmu.edu"],
+			"to": recipientList,
 			"subject": "Hello",
 			"template": "testing",
 			"v:test" : "test"
 			})
 
-
-send_template_message()
 
